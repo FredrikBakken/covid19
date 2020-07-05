fea3d3b686f28889eb832616a42877cabb967d4d
Fredrik Bakken <fredda10x@gmail.com>
2020-07-05 10:37:03 +0200

    Provinces handled for some countries, e.g. Denmark.

--- notebooks/helpers/new_cases.py
+++ notebooks/helpers/new_cases.py
@@ -12,8 +12,15 @@ def get_new_cases(selected_slug):
     cases_url = f"https://api.covid19api.com/country/{selected_slug}/status/confirmed?from={from_date}&to={to_date}"
     json_response = requests.get(url = cases_url).json()
     cases = pd.json_normalize(json_response)
-    new_cases = cases.Cases.max() - cases.Cases.min()
+    
+    province_cases = {}
+    provinces = cases.Province.unique().tolist()
+    
+    for province in provinces:
+        selected_province = cases.loc[cases.Province == province]
+        new_cases = new_cases = selected_province.Cases.max() - selected_province.Cases.min()
+        province_cases[province] = new_cases
     
     fourteen_days_ago = day_0 + timedelta(days=1)
     
-    return fourteen_days_ago, yesterday, new_cases
+    return fourteen_days_ago, yesterday, province_cases
